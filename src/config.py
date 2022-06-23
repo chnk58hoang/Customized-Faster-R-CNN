945a8ce19139c1c8832a2dffa554b28059cac40e
chnk58hoang <19021285@vnu.edu.vn>
2022-06-23 17:00:47 +0700

    cleancode

--- src/config.py
+++ src/config.py
@@ -13,9 +13,9 @@ OUT_DIR = '/kaggle/working/'
 EPOCHS = 200
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-BATCH_SIZE = 32  # increase / decrease according to GPU memeory
-RESIZE_TO = 512  # resize the image for training and transforms
+BATCH_SIZE = 32
+RESIZE_TO = 512
 
-# Images and labels direcotry should be relative to train.py
+
 TRAIN_IMAGES = '/kaggle/input/pascal-voc-2012/VOC2012/JPEGImages/'
 TRAIN_LABELS = '/kaggle/input/pascal-voc-2012/VOC2012/Annotations/'
